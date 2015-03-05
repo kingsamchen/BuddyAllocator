fde39ab7d53e0e271c561e999d263c72a61e6d63
Kingsley Chen <kingsamchen@gmail.com>
2015-03-05 17:57:54 +0800

    temporary save. continue when I am back to home.

--- src/memory_bin.h
+++ src/memory_bin.h
@@ -10,17 +10,22 @@
 #define MEMORY_BIN_H_
 
 #include <cstdint>
+#include <memory>
 
 #include "compiler_helper.h"
 
-template<size_t Granularity>
+template<typename BlockType, size_t Granularity>
 struct SlotTraits {
-    enum { granularity = Granularity };
-    uint8_t s[Granularity];
+    enum {
+        granularity = Granularity,
+        block_count = granularity / sizeof(BlockType)
+    };
+
+    BlockType s[block_count];
 };
 
 // Using 4K for a slot by default.
-using DefaultSlotTraits = SlotTraits<4096>;
+using DefaultSlotTraits = SlotTraits<uint8_t, 4096>;
 
 template<typename SlotTraitsType = DefaultSlotTraits>
 class MemoryBin {
@@ -28,7 +33,9 @@ private:
     using Slot = SlotTraitsType;
 
 public:
-    MemoryBin(size_t slot_count) {};
+    MemoryBin(size_t slot_count)
+        : slots_(std::make_unique<Slot[]>(slot_count))
+    {}
 
     MemoryBin(const MemoryBin&) = delete;
 
@@ -40,6 +47,8 @@ public:
 
     ~MemoryBin() = default;
 
+    Slot* Map(size_t index);
+
     size_t slot_granularity() const
     {
         return Slot::granularity;
@@ -47,6 +56,9 @@ public:
 
 private:
     FRIEND_TEST_WITH_PREFIX(MemoryBin);
+
+private:
+    std::unique_ptr<Slot[]> slots_;
 };
 
 #endif  // MEMORY_BIN_H_
